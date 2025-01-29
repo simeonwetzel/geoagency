cfe7611c9d63ebe6b75b9a01b3bb2a1c29ada1fd
simeonwetzel <simeon.wetzel@tu-dresden.de>
2025-01-29 21:22:20 +0100

    Added multi-agent setup. Changed the way the RepoRetriever gets initialized.

--- geoagency/agents/smolagent_base.py
+++ geoagency/agents/smolagent_base.py
@@ -1,13 +1,40 @@
-from smolagents import CodeAgent, HfApiModel
+from smolagents import CodeAgent, ToolCallingAgent, ManagedAgent
 import os
 from ..llm_manager import LLMManager
-from .tools.tools import retrieve_metadata, format_metadata_results
+from .tools.tools import MetadataRetrieverTool, clarify_search_criteria
 from .retriever.retriever import RepoRetriever
+from smolagents.prompts import MANAGED_AGENT_PROMPT
 
-
+# Specify the model to be used 
 llm = LLMManager.get_llm()
-agent = CodeAgent(tools=[retrieve_metadata, format_metadata_results], model=llm, additional_authorized_imports=["asyncio"])
 
-def call_agent(query: str, retriever: RepoRetriever) -> str:
-    return agent.run(query,
-                     additional_args={"retriever": retriever})
+# Create different agents
+retriever = RepoRetriever()
+metadata_retriever_agent = CodeAgent(
+    tools=[clarify_search_criteria, MetadataRetrieverTool(retriever)],
+    model=llm, 
+    additional_authorized_imports=["asyncio"],
+    max_steps=1
+)
+
+# # Make managed agents that will be used by the manager agent
+managed_metadata_agent = ManagedAgent(
+     agent=metadata_retriever_agent,
+     name="metadata_retriever",
+     description="Retrieves metadata from different repositories and outputs the top-5 results. For any query about data or metadata use this agent.",
+     managed_agent_prompt=MANAGED_AGENT_PROMPT + """Always use the query as is, do not modify it or hallucinate search criteria.""",
+     #additional_prompting
+)
+
+manager_agent = CodeAgent(
+    tools=[],
+    model=llm,
+    managed_agents=[managed_metadata_agent],
+    max_steps=1
+)
+manager_agent.system_prompt = manager_agent.system_prompt + """
+For each dataset/metadata query do a single request and show the results.
+"""
+
+def call_agent(query: str) -> str:
+    return manager_agent.run(query)
--- geoagency/agents/tools/tools.py
+++ geoagency/agents/tools/tools.py
@@ -1,38 +1,47 @@
-from smolagents import DuckDuckGoSearchTool, tool
+from smolagents import DuckDuckGoSearchTool, tool, Tool
 from loguru import logger
 from ..retriever.retriever import RepoRetriever
 
-@tool
-def retrieve_metadata(query: str, retriever: RepoRetriever) -> dict:
-    """
+class MetadataRetrieverTool(Tool):
+    name = "retrieve_metadata"
+    description = """
     This tool can be used to retrieve metadata from different repositories.
-
-    The output can be used to briefly summarize the top 5 results from the query.
-
-    Args:
-        query: The query string to search for.
-        retriever: An instance of the RepoRetriever class.
+    The output can be used to briefly summarize the top 3 results from the query.
     """
-    import asyncio
-    response = asyncio.run(retriever.query_all_repos(query))
+    inputs = {
+        "query": {
+            "type": "string",
+            "description": "The query string to search for.",
+        }
+    }
+    output_type = "object"
+    def __init__(self, retriever: RepoRetriever):
+        super().__init__()
+        self.retriever = retriever
 
-    top_5_results = response.get("results", [])[:5]
-    return top_5_results
+    def forward(self, query: str) -> dict:
+        import asyncio
+        response = asyncio.run(self.retriever.query_all_repos(query))
+
+        top_3_results = response.get("results", [])[:3]
+        
+        formatted_results = ""
+        for idx, result in enumerate(top_3_results):
+            formatted_results += f"Result {idx + 1}:\n"
+            formatted_results += f"Text: {result.get('text', 'N/A')}\n"
+            formatted_results += f"Source Repository: {result.get('source', 'N/A')}\n"
+            
+        return formatted_results
+
 
 @tool
-def format_metadata_results(results: dict) -> str:
+def clarify_search_criteria(query: str) -> str:
     """
-    This tool can be used to format the metadata results.
-
-    Also describe the metadata results.
-
+    Ask for clarification if the search criteria are not clear instead of conducting a search. 
+    
+    Consider spatial, thematic or temporal criteria depending on the context.
+    
     Args:
-        results: The metadata results to format.
+        query: The original query string.
     """
-    formatted_results = ""
-    for idx, result in enumerate(results):
-        formatted_results += f"Result {idx + 1}:\n"
-        formatted_results += f"Text: {result.get('text', 'N/A')}\n"
-        formatted_results += f"Source Repository: {result.get('source', 'N/A')}\n"
-
-    return formatted_results
+    return "Please provide more information to clarify the search criteria."
